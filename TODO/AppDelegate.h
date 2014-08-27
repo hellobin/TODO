6daf53b9445c54a211a05b90e0546da7c26bcda3
luobin <luobin@pinidea.com>
2014-08-27 14:52:21 +0800

    Initial Commit

---
+++ TODO/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TODO
+//
+//  Created by luobin on 14-8-27.
+//  Copyright (c) 2014年 pinidea.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TODO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TODO
+//
+//  Created by luobin on 14-8-27.
+//  Copyright (c) 2014年 pinidea.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
